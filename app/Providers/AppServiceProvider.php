7b4ae94dda9ba19b9fe750ff5bd90ebf4b941c7e
Harmandeep kaur <sidhuharman7890@gmail.com>
2026-05-29 23:37:57 +0530

    Force HTTPS for assets to fix broken CSS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Force HTTPS in production to fix mixed-content (CSS not loading) on Render
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
